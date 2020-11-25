97fef1b6afa12f65f8a3309552f5c55a7fbeda43
Kaikai8888 <wuky812@gmail.com>
2020-11-25 11:53:24 +0800

    feat: confirm user password and unique email when register

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
+
 const app = express()
 const port = 3000
 
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'fejwiovnur',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,11 +5,28 @@ const User = db.User
 let userController = {
   signUpPage: (req, res) => res.render('signup'),
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(() => res.redirect('/signin'))
+    if (req.body.password !== req.body.passwordCheck) {
+      req.flash('error_messages', '兩次密碼輸入不同!')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({ where: { email: req.body.email } })
+        .then(user => {
+          if (user) {
+            req.flash('error_messages', '信箱重複！')
+            return res.redirect('/signup')
+          } else {
+            User.create({
+              name: req.body.name,
+              email: req.body.email,
+              password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+            }).then(() => {
+              req.flash('success_messages', '成功註冊帳號！')
+              return res.redirect('/signin')
+            })
+          }
+        })
+        .catch(error => console.log(error))
+    }
   }
 }
 
