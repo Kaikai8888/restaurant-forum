063f6d4a11f0fc62393596a6fbdf98f8d9136e8e
Kaikai8888 <wuky812@gmail.com>
2020-12-02 10:24:28 +0800

    feat: create comment model and it's relationship relationship between restaurant and comment, user and comment

---
+++ migrations/20201202020755-create-comment.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      UserId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      RestaurantId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'Restaurants',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
